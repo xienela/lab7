091bc0109913b791e8858d15aa1a5d173619ea34
xienela <72793304+xienela@users.noreply.github.com>
2023-03-24 01:37:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,61 @@
+from queue import PriorityQueue
+
+def a_star_search(start, goal, graph):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = dict()
+    cost_so_far = dict()
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + graph.heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    return came_from, cost_so_far
+
+
+class PacmanGraph:
+    def __init__(self, walls):
+        self.walls = walls
+
+    def in_bounds(self, pos):
+        x, y = pos
+        return 0 <= x < self.walls.width and 0 <= y < self.walls.height
+
+    def passable(self, pos):
+        x, y = pos
+        return not self.walls[x][y]
+
+    def neighbors(self, pos):
+        x, y = pos
+        neighbors = [(x + dx, y + dy) for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]]
+        neighbors = filter(self.in_bounds, neighbors)
+        neighbors = filter(self.passable, neighbors)
+        return list(neighbors)
+
+    def cost(self, current, next):
+        return 1
+
+    def heuristic(self, goal, next):
+        return manhattanDistance(goal, next)
+
 class SimpleExtractor(FeatureExtractor):
     def getFeatures(self, state, action):
         # Extract the grid of walls
         walls = state.getWalls()
+        graph = PacmanGraph(walls)
+
         ghosts = state.getGhostStates()
         ghost_positions = [ghost.getPosition() for ghost in ghosts]
         ghost_distances = [manhattanDistance(ghost_position, state.getPacmanPosition()) for ghost_position in ghost_positions]
@@ -15,41 +69,11 @@ class SimpleExtractor(FeatureExtractor):
         dx, dy = Actions.directionToVector(action)
         next_x, next_y = int(x + dx), int(y + dy)
 
-        # Calculate the nearest food distance
+         # Calculate the nearest food distance
         food = state.getFood()
-        food_distances = [manhattanDistance((next_x, next_y), (i, j)) for i in range(walls.width) for j in range(walls.height) if food[i][j]]
+        food_list = [(i, j) for i in range(walls.width) for j in range(walls.height) if food[i][j]]
+        food_distances = [len(a_star_search((next_x, next_y), food_pos, graph)[0]) for food_pos in food_list]
         if food_distances:
-            features["closest-food"] = float(min(food_distances)) / (walls.width * walls.height)
+            features["closest-food"] = min(food_distances)
 
-        # Calculate the nearest ghost distance
-        next_ghost_distances = [manhattanDistance(ghost_position, (next_x, next_y)) for ghost_position in ghost_positions]
-        if next_ghost_distances:
-            features["closest-ghost"] = float(min(next_ghost_distances)) / (walls.width * walls.height)
-
-        # Calculate the nearest scared ghost distance
-        scared_ghosts = [ghost for ghost in ghosts if ghost.scaredTimer > 0]
-        scared_ghost_positions = [ghost.getPosition() for ghost in scared_ghosts]
-        scared_ghost_distances = [manhattanDistance(ghost_position, (next_x, next_y)) for ghost_position in scared_ghost_positions]
-        if scared_ghost_distances:
-            features["closest-scared-ghost"] = float(min(scared_ghost_distances)) / (walls.width * walls.height)
-
-        # Calculate the distance to the nearest capsule
-        capsules = state.getCapsules()
-        capsule_distances = [manhattanDistance((next_x, next_y), capsule) for capsule in capsules]
-        if capsule_distances:
-            features["closest-capsule"] = float(min(capsule_distances)) / (walls.width * walls.height)
-
-        # Calculate the distance to the nearest dead end
-        dead_end_distances = []
-        for i in range(walls.width):
-            for j in range(walls.height):
-                if walls[i][j] == False:
-                    neighbors = [(i + dx, j + dy) for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]]
-                    walls_count = sum(1 for nx, ny in neighbors if walls[nx][ny])
-                    if walls_count == 3:
-                        dead_end_distances.append(manhattanDistance((next_x, next_y), (i, j)))
-        if dead_end_distances:
-            features["closest-dead-end"] = float(min(dead_end_distances)) / (walls.width * walls.height)
-
-        features.divideAll(10.0)
         return features
