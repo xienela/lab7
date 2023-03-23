ff97b4ee6c0d5080709d9ef87f08ea2aa48b3406
xienela <72793304+xienela@users.noreply.github.com>
2023-03-24 00:56:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -219,4 +219,52 @@ def computeActionFromQValues(self, state):
         return random.choice(max_actions)
     else:
         return legal_actions[0]  # If max_actions is empty, return the first legal action
+      
+def getFeatures(self, state, action):
+        # extract the grid of food and wall locations and get the ghost locations
+        food = state.getFood()
+        capsules = state.getCapsules()
+        walls = state.getWalls()
+        ghosts = state.getGhostPositions()
+
+        features = util.Counter()
+
+        features["bias"] = 1.0
+
+        # compute the location of pacman after he takes the action
+        x, y = state.getPacmanPosition()
+        dx, dy = Actions.directionToVector(action)
+        next_x, next_y = int(x + dx), int(y + dy)
+
+        # check for imminent ghost collisions
+        features["#-of-not-scared-ghosts-1-step-away"] = sum((next_x, next_y) in Actions.getLegalNeighbors(g, walls) for g in ghosts)
+
+        # check for ghosts one step away
+        features["#-of-not-scared-ghosts-2-step-away"] = sum(1 for g in ghosts if manhattanDistance((next_x, next_y), g) == 2)
+
+        # check if pacman will eat food
+        if food[next_x][next_y]:
+            features["eats-food"] = 1.0
+
+        # compute the distance to the closest food
+        dist = closestFood((next_x, next_y), food, walls)
+        if dist is not None:
+            # make the distance a number less than one otherwise the update
+            # will diverge wildly
+            features["closest-food"] = float(dist) / (walls.width * walls.height)
+
+        # check if pacman will eat a capsule
+        if (next_x, next_y) in capsules:
+            features["eats-capsule"] = 1.0
+
+        # count the number of remaining food
+        features["remaining-food"] = float(food.count()) / (walls.width * walls.height)
+
+        # count the number of remaining capsules
+        features["remaining-capsules"] = float(len(capsules)) / (walls.width * walls.height)
+
+        # normalize the features
+        features.divideAll(10.0)
+
+        return features
 
